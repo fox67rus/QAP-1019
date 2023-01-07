020aaebbe7eb42c1680c867fb93493c0b480ba05
Elena <fox67rus@mail.ru>
2023-01-07 17:41:02 +0300

    Модуль 13 - ЗАДАНИЕ 13.8.19 (HW-03)

---
+++ module13/HW_03.py
@@ -0,0 +1,14 @@
+# ЗАДАНИЕ 13.8.19 (HW-03)
+
+total_cost = 0
+number_of_tickets = int(input("Введите количество билетов, которое вы хотите приобрести: "))
+
+for visitor in range(1, number_of_tickets + 1):
+    visitor_age = int(input(f"Введите возраст {visitor}-го посетителя: "))
+    if visitor_age < 18:
+        continue
+    elif 18 <= visitor_age < 25:
+        total_cost += 990
+    else:
+        total_cost += 1390
+print(total_cost * 0.9 if number_of_tickets > 3 else total_cost)
