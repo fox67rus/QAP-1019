ed02ec4cc5d9559bbd191b650d070c823e0625f2
Elena <fox67rus@mail.ru>
2023-01-08 18:58:38 +0300

    Модуль 14 - Рекурсия

---
+++ module14/task14_2_5.py
@@ -0,0 +1,10 @@
+# Задание 14.2.5
+# Дано натуральное число N. Вычислите сумму его цифр.
+def rec_num_sum(num):
+    if num // 10 == 0:
+        return num
+    return num % 10 + rec_num_sum(num // 10)
+
+print(rec_num_sum(2))
+print(rec_num_sum(12))
+print(rec_num_sum(126))
