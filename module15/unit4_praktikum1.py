eae7fa14a91de9fce5ad4c2a2edaeafa94615c8f
Elena <fox67rus@mail.ru>
2023-01-15 16:45:35 +0300

    Добавлена функция вывода наиболее повторяющихся слов, с ограничением в виде количества символов в слове. + небольшие поправки

--- module15/unit4_praktikum1.py
+++ module15/unit4_praktikum1.py
@@ -31,35 +31,62 @@ def prepare_text_from_file(file_, info: bool = False) -> list:
 
 
 def delete_special_char(text: str) -> str:
-    """ Удаляем все переносы и спецсимволы из текста text и возвращает текст без спецсимволов"""
+    """ Удаляет все переносы и спецсимволы из текста text и возвращает текст без спецсимволов"""
     marks = ['!', '(', ')', '-', ';', '?', ':', '\n', ',', '.', '«', '»']
     for mark in marks:
         text = text.replace(mark, '')
     return text
 
 
-def count_words(words: list) -> dict:
-    """ Считает количество повторений слов в списке. На выходе словарь вида 'слово': количество повторений в списке """
+def count_rep_words(words: list, min_number_char: int = 3) -> dict:
+    """
+    Считает количество повторений слов в списке, если слово длиннее определенного количество букв.
+    :param words: список слов
+    :param min_number_char: минимальное количество символов в слове
+    :return: словарь вида 'слово': количество повторений в списке
+    """
     count = {}  # для подсчета слов и их количества
     for word in words:
-        if word in count:  # если слово уже встречалось, то увеличиваем его количество на 1
-            count[word] += 1
-        else:
-            count[word] = 1
-
-    # for word, cnt in count.items():
-    #     print(f'Слово "{word}" встречается {cnt} раз')
+        if len(word) > min_number_char:
+            if word in count:  # если слово уже встречалось, то увеличиваем его количество на 1
+                count[word] += 1
+            else:
+                count[word] = 1
 
     return count
 
+def search_max_value(input_dict:dict) -> dict:
+    if input_dict:
+        max_frequent_value = max(input_dict.values())
+        out_dict = {k: v for k, v in input_dict.items() if v == max_frequent_value}
+        count_word = len(out_dict)
+        if count_word > 1:
+            print(f'Найдено несколько слов ({count_word} шт.) удовлетворяющим условиям поиска:')
+            for word, cnt in out_dict.items():
+                print(f'Слово - "{word}", количество повторений в тексте - {cnt}')
+
+        return out_dict
+    else:
+        print('Не найдено слов, с выбранными параметрами. Проверьте ограничение количества букв')
+
 
 def is_english(text: str) -> bool:
-    """ Определяет на английском языке слово или нет """
+    """ Возвращает True если слово на английском языке """
     pass
 
 
 if __name__ == '__main__':
     # filename = input("Введите имя файла, например - en_rus.txt:\n")
+    # char_limit = input("Минимальное количество символов в слове: \n")
     filename = 'en_rus.txt'
+    char_limit = 3
+
+
+
+
     all_words = prepare_text_from_file(filename, True)
-    print(count_words(all_words))
+
+    search_max_value(count_rep_words(all_words, char_limit))
+
+
+
