440ee7b73a18dedc97b871d2ae658ace313ec9a4
Elena <fox67rus@mail.ru>
2023-01-15 19:45:09 +0300

    Добавлена функция определения на английском ли слово и функция поиска максимального слова на английском.

--- module15/unit4_praktikum1.py
+++ module15/unit4_praktikum1.py
@@ -41,7 +41,7 @@ def delete_special_char(text: str) -> str:
 def count_rep_words(words: list, min_number_char: int = 3) -> dict:
     """
     Считает количество повторений слов в списке, если слово длиннее определенного количество букв.
-    :param words: список слов
+    :param words: Список слов
     :param min_number_char: минимальное количество символов в слове
     :return: словарь вида 'слово': количество повторений в списке
     """
@@ -52,10 +52,15 @@ def count_rep_words(words: list, min_number_char: int = 3) -> dict:
                 count[word] += 1
             else:
                 count[word] = 1
-
     return count
 
-def search_max_value(input_dict:dict) -> dict:
+
+def search_max_value(input_dict: dict) -> dict:
+    """
+    Поиск ключей (слов) наиболее часто встречающихся в словаре
+    :param input_dict:
+    :return: словарь с найденным словом или словами, если несколько слов имеют одинаковое количество повторений
+    """
     if input_dict:
         max_frequent_value = max(input_dict.values())
         out_dict = {k: v for k, v in input_dict.items() if v == max_frequent_value}
@@ -72,21 +77,61 @@ def search_max_value(input_dict:dict) -> dict:
 
 def is_english(text: str) -> bool:
     """ Возвращает True если слово на английском языке """
+    try:
+        text.encode(encoding='utf-8').decode('ascii')
+    except UnicodeDecodeError:
+        return False
+    else:
+        return True
+
+
+def search_max_length(input_dict: dict) -> list:
+    """
+    Поиск самого длинного слова на английском
+    :param input_dict:
+    :return: возвращает список кортежей вида ('слово', количество символов) со словом или словами, если найдено
+             несколько слов с одинаковым количеством символов
+    """
+    if input_dict:
+        words_list = list(input_dict)
+        eng_word_list = []
+        for word in words_list:
+            if is_english(word):
+                eng_word_list.append(word)
+
+        print(f'\nСписок ({len(eng_word_list)}) английских слов - {eng_word_list})')
+
+        max_length = 0
+        out_list = []
+        for word in eng_word_list:
+            if len(word) > max_length:
+                out_list = []
+                max_length = len(word)
+                out_list.append((word, max_length))
+            elif len(word) == max_length:
+                out_list.append((word, max_length))
+
+        count_word = len(out_list)
+        if count_word > 1:
+            print(f'Найдено несколько слов ({count_word} шт.) с одинаковым количеством символов:')
+            for word, length in out_list:
+                print(f'Слово - "{word}", количество символов - {length}')
+        else:
+            print(f'Самое длинное слово на английском - {out_list[0][0]}. В нём {out_list[0][1]} букв')
+        return out_list
+
     pass
+
+
+def main(textfile: str):
+    all_words = prepare_text_from_file(textfile)
+    word_with_rep = count_rep_words(all_words)
+    search_max_value(word_with_rep)
+    search_max_length(word_with_rep)
 
 
 if __name__ == '__main__':
     # filename = input("Введите имя файла, например - en_rus.txt:\n")
     # char_limit = input("Минимальное количество символов в слове: \n")
     filename = 'en_rus.txt'
-    char_limit = 3
-
-
-
-
-    all_words = prepare_text_from_file(filename, True)
-
-    search_max_value(count_rep_words(all_words, char_limit))
-
-
-
+    main(filename)
