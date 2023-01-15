2dc4766a1efc7237271691d6c7a5a126f3712cd3
Elena <fox67rus@mail.ru>
2023-01-15 11:58:45 +0300

    считывание файла отдельной функцией

--- module15/unit4_praktikum1.py
+++ module15/unit4_praktikum1.py
@@ -3,38 +3,44 @@
 # и наиболее длинное слово на английском языке.
 # В файле ожидается смешанный текст на двух языках — русском и английском.
 
-def is_english(text:str) -> bool:
-    """ Определяет на английском языке слово или нет """
-    pass
+def prepare_text_from_file(file_) -> list:
+    """ Обрабатывает текст из файла """
+    words_list = []
+    with open(file_, encoding='utf-8') as file:
+        for line in file:
+            line = line.split(' ')  # list
 
-def delete_special_char(text:str) -> str:
+            for word in line:  # str
+                word = word.lower()
+                if not word.isalnum():
+                    word = delete_special_char(word)
+
+            # пропускаем пустые слова
+            if word:
+                words_list.append(word)
+        return words_list
+
+
+def delete_special_char(text: str) -> str:
     """ Удаляем все переносы и спецсимволы из текста text и возвращает текст без спецсимволов"""
     marks = ['!', '(', ')', '-', ';', '?', ':', '\n', ',', '.', '«', '»']
     for mark in marks:
         text = text.replace(mark, '')
     return text
 
-def prepare_text_from_file(file_):
-    """ Обрабатывает текст из файла """
+
+def count_words(words: list) -> dict:
+    """ Считает количество повторений слов в списке. На выходе словарь вида 'слово': количество повторений """
     pass
 
 
-# filename = input("Введите имя файла, например - en_rus.txt:\n")
-filename = 'en_rus.txt'
-all_words = []
+def is_english(text: str) -> bool:
+    """ Определяет на английском языке слово или нет """
+    pass
 
 
-with open(filename, encoding='utf-8') as file:
-    for line in file:
-        line = line.split(' ')  # list
-
-        for word in line:  # str
-            word = word.lower()
-            if not word.isalnum():
-                word = delete_special_char(word)
-
-        # пропускаем пустые слова
-        if word:
-            all_words.append(word)
-
+if __name__ == '__main__':
+    # filename = input("Введите имя файла, например - en_rus.txt:\n")
+    filename = 'en_rus.txt'
+    all_words = prepare_text_from_file(filename)
     print(all_words)
