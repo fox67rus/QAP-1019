dc29e7eb7cfe1aefaa2c4b78e8e22cc85d816b9b
Elena <fox67rus@mail.ru>
2023-01-18 19:03:14 +0300

    Добавлена проверка, что файл не пустой; обработка ошибок при открытии файла; изменена функция поиска наиболее частых слов; доработки

--- module15/praktikum1/unit4_praktikum1.py
+++ module15/praktikum1/unit4_praktikum1.py
@@ -11,28 +11,35 @@ def prepare_text_from_file(file_, info: bool = False) -> list:
     :return: возвращает список не пустых слов в нижнем регистре, без спецсимволов
     """
     words_list = []
-    with open(file_, encoding='utf-8') as file:  # добавить обработку FileNotFoundError
-        for line in file:
-            line = line.split(' ')  # list
-
-            for word in line:  # str
-                word = word.lower()
-                if not word.isalnum():
-                    word = delete_special_char(word)
-
-                # добавляем в список, пропуская пустые слова
-                if word:
-                    words_list.append(word)
-        if info:
-            print(f'Сформирован список всех слов из полученного текста:\n{words_list}')
-            print(f'Всего слов в тексте - {len(words_list)}, в том числе уникальных - {len(list(set(words_list)))}\n')
-
-        return words_list
+    try:
+        with open(file_, encoding='utf-8') as file:
+            # Считываем содержимое файла в переменную, чтобы была возможность проверки не пустой ли он
+            text = file.readlines()
+    except FileNotFoundError as e:
+        print('Ошибка открытия файла. Проверьте путь или имя файла', e)
+    else:
+        if len(text) > 1:
+            for line in text:
+                line = line.split(' ')
+                for word in line:  # str
+                    word = word.lower()
+                    if not word.isalnum():
+                        word = delete_special_char(word)
+                    # добавляем в список, пропуская пустые слова
+                    if word:
+                        words_list.append(word)
+            if info:
+                print(f'Сформирован список всех слов из полученного текста:\n{words_list}')
+                print(
+                    f'Всего слов в тексте - {len(words_list)}, в том числе уникальных - {len(list(set(words_list)))}\n')
+        else:
+            print('Файл пустой')
+    return words_list
 
 
 def delete_special_char(text: str) -> str:
     """ Удаляет все переносы и спецсимволы из текста text и возвращает текст без спецсимволов"""
-    marks = ['!', '(', ')', '-', ';', '?', ':', '\n', ',', '.', '«', '»']
+    marks = ['!', '(', ')', ';', '?', ':', '\n', ',', '.', '«', '»']
     for mark in marks:
         text = text.replace(mark, '')
     return text
@@ -55,24 +62,28 @@ def count_rep_words(words: list, min_number_char: int = 3) -> dict:
     return count
 
 
-def search_max_value(input_dict: dict) -> dict:
+def search_max_value(input_dict: dict) -> list:
     """
     Поиск ключей (слов) наиболее часто встречающихся в словаре
     :param input_dict:
-    :return: словарь с найденным словом или словами, если несколько слов имеют одинаковое количество повторений
+    :return: список кортежей с наиболее частым словом или словами, если несколько слов имеют одинаковое количество
+             повторений
     """
     if input_dict:
         max_frequent_value = max(input_dict.values())
-        out_dict = {k: v for k, v in input_dict.items() if v == max_frequent_value}
-        count_word = len(out_dict)
-        if count_word > 1:
-            print(f'Найдено несколько слов ({count_word} шт.) удовлетворяющим условиям поиска:')
-            for word, cnt in out_dict.items():
-                print(f'Слово - "{word}", количество повторений в тексте - {cnt}')
+        out_list = [(k, v) for k, v in input_dict.items() if v == max_frequent_value]
 
-        return out_dict
+        count_word = len(out_list)
+        if count_word > 1:
+            print(f'Найдено несколько слов ({count_word} шт.) с одинаковым количеством повторений:')
+            for word, cnt in out_list:
+                print(f'Слово - "{word}", количество повторений в тексте - {cnt}')
+        else:
+            print(
+                f'Самое часто встречающееся слово в тексте - {out_list[0][0]}.  Найдено - {out_list[0][1]} повторений')
+        return out_list
     else:
-        print('Не найдено слов, с выбранными параметрами. Проверьте ограничение количества букв')
+        print('Не найдено слов, с выбранными параметрами.')
 
 
 def is_english(text: str) -> bool:
@@ -85,7 +96,7 @@ def is_english(text: str) -> bool:
         return True
 
 
-def search_max_length(input_dict: dict) -> list:
+def search_longest_word(input_dict: dict) -> list:
     """
     Поиск самого длинного слова на английском
     :param input_dict:
@@ -99,7 +110,8 @@ def search_max_length(input_dict: dict) -> list:
             if is_english(word):
                 eng_word_list.append(word)
 
-        print(f'\nСписок ({len(eng_word_list)}) английских слов - {eng_word_list})')
+        print('\n')
+        # print(f'\nСписок ({len(eng_word_list)}) английских слов - {eng_word_list})')
 
         max_length = 0
         out_list = []
@@ -119,19 +131,22 @@ def search_max_length(input_dict: dict) -> list:
         else:
             print(f'Самое длинное слово на английском - {out_list[0][0]}. В нём {out_list[0][1]} букв')
         return out_list
-
-    pass
+    else:
+        print('Нет данных для обработки')
 
 
 def main(textfile: str):
     all_words = prepare_text_from_file(textfile)
-    word_with_rep = count_rep_words(all_words)
-    search_max_value(word_with_rep)
-    search_max_length(word_with_rep)
+    if all_words:
+        word_with_rep = count_rep_words(all_words)
+        search_max_value(word_with_rep)
+        search_longest_word(word_with_rep)
 
 
 if __name__ == '__main__':
     # filename = input("Введите имя файла, например - en_rus.txt:\n")
     # char_limit = input("Минимальное количество символов в слове: \n")
     filename = 'en_rus.txt'
+    # filename = 'пустой.txt'
+
     main(filename)
