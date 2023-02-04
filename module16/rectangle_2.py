c3f511bde061a9ed25fc8c5b1c50f10412ae3964
Elena <fox67rus@mail.ru>
2023-02-04 15:06:54 +0300

    Задание 16.8.2

--- module16/rectangle_2.py
+++ module16/rectangle_2.py
@@ -3,13 +3,8 @@ from rectangle_class import Rectangle, Square, Circle
 rect_1 = Rectangle(3, 4)
 rect_2 = Rectangle(12, 5)
 
-# print(rect_1.get_area())
-# print(rect_2.get_area())
-
 square_1 = Square(5)
 square_2 = Square(10)
-
-# print(square_1.get_area_square(), square_2.get_area_square())
 
 circle_1 = Circle(4)
 circle_2 = Circle(7)
