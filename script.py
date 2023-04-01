ecc569a0da3a96167737cc53b0a6995419829c1d
Elena <fox67rus@mail.ru>
2023-04-01 16:19:50 +0300

    21.3. Декорирование функций с аргументами

--- script.py
+++ script.py
@@ -1,5 +1,19 @@
-for a in range(1, 8):
-    for b in range(1, 8):
-        for c in range(1, 10):
-            if a + b + c == 13:
-                print(f'{a=}, {b=}, {c=}, {a * b * c=}')
+import functools
+
+def do_twice(func):
+  @functools.wraps(func)
+  def wrapper(*args, **kwargs):
+      func(*args, **kwargs)
+      return func(*args, **kwargs)
+  return wrapper
+
+
+
+
+@do_twice
+def t_twice(str):
+    print("Этот вызов возвращает строку {0}".format(str))
+    return "Done"
+
+print(t_twice.__name__)
+print(do_twice.__name__)
