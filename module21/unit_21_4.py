75cfd329783ce297f0b1fc9f448b07adab1b58f7
Elena <fox67rus@mail.ru>
2023-04-01 16:18:39 +0300

    21.4.1. Декорирование функций с аргументами. Знакомство с фикстурами.

---
+++ module21/unit_21_4.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+
+import pytest
+import requests
+
+
+@pytest.fixture()
+def some_data():
+    return 42
+
+def test_some_data(some_data):
+    assert some_data == 42
+
+@pytest.fixture(autouse=True)
+def get_key():
+    # переменные email и password нужно заменить своими учетными данными
+    email = 'test@qiott.com'
+    password = 'Qwerty_123'
+    response = requests.post(url='https://petfriends.skillfactory.ru/login',
+                             data={"email": email, "pass": password})
+    assert response.status_code == 200, 'Запрос выполнен неуспешно'
+    assert 'Cookie' in response.request.headers, 'В запросе не передан ключ авторизации'
+    return response.request.headers.get('Cookie')
+
+def test_getAllPets(get_key):
+    response = requests.get(url='https://petfriends.skillfactory.ru/api/pets',
+                            headers={"Cookie": get_key})
+    assert response.status_code == 200, 'Запрос выполнен неуспешно'
+    assert len(response.json().get('pets')) > 0, 'Количество питомцев не соответствует ожиданиям'
+
+@pytest.fixture(autouse=True)
+def time_delta():
+    start_time = datetime.now()
+    yield  # вызвать выполнение тестовой функции посреди фикстуры и продолжить после завершения работы функции
+    end_time = datetime.now()
+    print (f"\nТест шел: {end_time - start_time}")
