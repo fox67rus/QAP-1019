3ca430f089303de214b20a2808233db71f43cd13
Elena <fox67rus@mail.ru>
2023-06-15 18:10:00 +0300

    Вебинар «WorkShop по работе с локаторами и прерываниями»

--- module23/test.py
+++ module23/test.py
@@ -27,17 +27,17 @@ class TestLoginStellarBurgers:
         self.driver.find_element(By.XPATH, "//input[@type='password']").send_keys(self.password)
         self.driver.find_element(By.XPATH, "//button[contains(text(),'Войти')]").click()
         # проверяем, что после авторизации появилась кнопка Оформить заказ
-        assert WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'Оформить заказ')]")))
-
+        assert WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'Оформить заказ')]")))
 
     def test_login_by_button_in_form(self):
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH,
-             "//button[contains(text(),'Войти в аккаунт')]"))).click()
+                                                                         "//button[contains(text(),'Войти в аккаунт')]"))).click()
         self.login()
 
     def test_login_from_cabinet(self):
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(((By.XPATH,
-            "//p[contains(text(),'Личный Кабинет')]")))).click()
+                                                                          "//p[contains(text(),'Личный Кабинет')]")))).click()
         self.login()
 
     def test_registration(self):
@@ -47,18 +47,18 @@ class TestLoginStellarBurgers:
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(
             (By.XPATH, "//button[contains(text(),'Войти в аккаунт')]"))).click()
         WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(
-                (By.XPATH, "//a[@href='/register']"))).click()
+            (By.XPATH, "//a[@href='/register']"))).click()
         WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(
-                (By.XPATH, "//label[text() = 'Имя']/following-sibling::input"))).send_keys(fake_name)
+            (By.XPATH, "//label[text() = 'Имя']/following-sibling::input"))).send_keys(fake_name)
         WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(
-                (By.XPATH, "//label[text() = 'Email']/following-sibling::input"))).send_keys(fake_email)
+            (By.XPATH, "//label[text() = 'Email']/following-sibling::input"))).send_keys(fake_email)
         WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(
-                (By.XPATH, "//input[@type='password']"))).send_keys(fake_password)
+            (By.XPATH, "//input[@type='password']"))).send_keys(fake_password)
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(
             (By.XPATH, "//button[contains(text(),'Зарегистрироваться')]"))).click()
         # self.login()
 
-        sleep(5) # плохой способ сделать задержку, чтобы увидеть заполнение данных
+        # sleep(5) # плохой способ сделать задержку, чтобы увидеть заполнение данных
 
     def teardown(self):
         self.close()
