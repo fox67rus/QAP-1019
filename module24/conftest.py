62f6b652751bb3b0e79077a222bd76c2f8a21099
Elena <fox67rus@mail.ru>
2023-06-21 16:28:03 +0300

    опции

--- module24/conftest.py
+++ module24/conftest.py
@@ -1,19 +1,21 @@
 import pytest
 import uuid
 
-@pytest.fixture
-def firefox_options(firefox_options):
-    firefox_options.binary = "C:\Program Files\Mozilla Firefox\firefox.exe"
-    firefox_options.add_argument('-foreground') # запуск в реальном режиме, для фонового - '-background'
-    firefox_options.set_preference('browser.anchor_color', '#FF0000') # выбор цвета подложки браузера
-    return firefox_options
+# @pytest.fixture
+# def firefox_options(firefox_options):
+#     firefox_options.binary = "C:\Program Files\Mozilla Firefox\firefox.exe"
+#     firefox_options.add_argument('-foreground') # запуск в реальном режиме, для фонового - '-background'
+#     firefox_options.set_preference('browser.anchor_color', '#FF0000') # выбор цвета подложки браузера
+#     return firefox_options
 @pytest.fixture
 def chrome_options(chrome_options):
-    chrome_options.binary = 'C:\Program Files\Google\Chrome\Application\chrome.exe'
+    chrome_options.binary_location = 'C:\Program Files\Google\Chrome\Application\chrome.exe'
     # chrome_options.add_extension('/path/to/extension.crx') # включение дополнений браузера
-    chrome_options.add_argument('--kiosk') # защищённый полноэкранный режим без меню, применяемый в публичных местах
+    # chrome_options.add_argument('--kiosk') # защищённый полноэкранный режим без меню, применяемый в публичных местах
+    # chrome_options.add_argument("--headless") # запуск без интерфейса
     return chrome_options
 
+# трюки
 @pytest.hookimpl(hookwrapper=True, tryfirst=True)
 def pytest_runtest_makereport(item, call):
     # This function helps to detect that some test failed
@@ -51,5 +53,4 @@ def web_browser(request, selenium):
 
         except:
             pass # just ignore any errors here
-
 
