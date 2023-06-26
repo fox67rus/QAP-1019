bcdc450ea93e922481713dab0efd63eefae294fc
Elena <fox67rus@mail.ru>
2023-06-26 11:54:15 +0300

    25.3. Множественный поиск элементов с помощью Selenium

---
+++ test1.py
@@ -0,0 +1,38 @@
+# run test:
+# cd module25
+# python -m pytest -v --driver Chrome --driver-path E:\Project\Python\skillfactory\drivers\chromedriver.exe test_PF_25_3.py
+
+from time import sleep
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+@pytest.fixture(autouse=True)
+def testing():
+   pytest.driver = webdriver.Chrome('\drivers\chromedriver.exe')
+
+   # Переходим на страницу авторизации
+   pytest.driver.get('https://petfriends.skillfactory.ru/login')
+
+   yield
+
+   pytest.driver.quit()
+
+
+def test_show_my_pets():
+   # Вводим email
+   pytest.driver.find_element(By.ID, "email").send_keys('vasya@mail.com')
+   sleep(1)
+
+   # Вводим пароль
+   pytest.driver.find_element(By.ID, "pass").send_keys("12345")
+   sleep(1)
+
+   # Нажимаем на кнопку входа в аккаунт
+   pytest.driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
+   sleep(1)
+
+   # Проверяем, что мы оказались на главной странице пользователя
+   assert pytest.driver.find_element(By.TAG_NAME, "h1").text == "PetFriends"
