03c83ffe75ba2b93c664e1f562f72ec441f43840
Elena <fox67rus@mail.ru>
2023-06-27 19:45:57 +0300

    практика

--- test1.py
+++ test1.py
@@ -1,38 +1,53 @@
 # run test:
-# cd module25
-# python -m pytest -v --driver Chrome --driver-path E:\Project\Python\skillfactory\drivers\chromedriver.exe test_PF_25_3.py
+# python -m pytest -v --driver Chrome --driver-path test1.py
 
 from time import sleep
 
 import pytest
 from selenium import webdriver
+from selenium.webdriver import Keys, ActionChains
+from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
 from selenium.webdriver.common.by import By
 
 
 @pytest.fixture(autouse=True)
 def testing():
-   pytest.driver = webdriver.Chrome('\drivers\chromedriver.exe')
+    pytest.driver = webdriver.Chrome('\drivers\chromedriver.exe')
+    pytest.driver.set_window_size(1400, 1000)
 
-   # Переходим на страницу авторизации
-   pytest.driver.get('https://petfriends.skillfactory.ru/login')
+    # Переходим на страницу авторизации
+    pytest.driver.get('https://www.wildberries.ru/')
 
-   yield
+    yield
 
-   pytest.driver.quit()
+    pytest.driver.quit()
 
 
-def test_show_my_pets():
-   # Вводим email
-   pytest.driver.find_element(By.ID, "email").send_keys('vasya@mail.com')
-   sleep(1)
+def test_search_fruto_nyanya():
+    sleep(3)
+    pytest.driver.implicitly_wait(10)
+    search = pytest.driver.find_element(By.ID, "searchInput")
+    query_text = 'фруктовое пюре в мягкой упаковке фрутоняня'
+    search.send_keys(query_text)
+    sleep(1)
+    search.send_keys(Keys.RETURN)
+    assert pytest.driver.find_element(By.TAG_NAME, "h1").text == f"По запросу «{query_text}» найдено"
+    sleep(5)
 
-   # Вводим пароль
-   pytest.driver.find_element(By.ID, "pass").send_keys("12345")
-   sleep(1)
 
-   # Нажимаем на кнопку входа в аккаунт
-   pytest.driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
-   sleep(1)
 
-   # Проверяем, что мы оказались на главной странице пользователя
-   assert pytest.driver.find_element(By.TAG_NAME, "h1").text == "PetFriends"
+    # footer = pytest.driver.find_element(By.TAG_NAME, "footer")
+    # scroll_origin = ScrollOrigin.from_element(footer, 0, -50)
+    # ActionChains(pytest.driver) \
+    #     .scroll_from_origin(scroll_origin, 0, 200) \
+    #     .perform()
+    prices = pytest.driver.find_elements(By.CLASS_NAME, "price__lower-price")
+    print(len(prices))
+    price_list = []
+    for el in prices:
+        price_list.append(el.text)
+
+    print(price_list)
+
+
+
