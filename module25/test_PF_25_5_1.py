78da770e8e3b253da80f921a5f9d029871bb8ed3
Elena <fox67rus@mail.ru>
2023-06-28 19:20:29 +0300

    явные ожидания для загрузки фото, корректировка кода и удаление лишнего

--- module25/test_PF_25_5_1.py
+++ module25/test_PF_25_5_1.py
@@ -36,18 +36,18 @@ def login():
     pytest.driver.quit()
 
 def test_my_pets_page():
+    pytest.driver.implicitly_wait(10)  # настройка времени неявных ожиданий
     pytest.driver.find_element(By.LINK_TEXT, 'Мои питомцы').click()
     assert pytest.driver.current_url == "https://petfriends.skillfactory.ru/my_pets"
 
     user_info = pytest.driver.find_element(By.CSS_SELECTOR,"div.task3.fill > div")
     expected_pets = int(user_info.text.split('\n')[1][-1])
 
-    pytest.driver.implicitly_wait(10)  # настройка времени неявных ожиданий
     actual_pets = WebDriverWait(pytest.driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, '//tbody/tr')))
 
     assert expected_pets == len(actual_pets), "Присутствуют не все питомцы"
 
-    pets_photos = pytest.driver.find_elements(By.CSS_SELECTOR, 'th > img')
+    pets_photos = WebDriverWait(pytest.driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'th > img')))
     print(len(pets_photos))
     pets_with_photos = 0
     for img in range(len(pets_photos)):
@@ -55,31 +55,15 @@ def test_my_pets_page():
             pets_with_photos += 1
     assert pets_with_photos >= expected_pets / 2, "Больше половины питомцев без фото"
 
-
     pets_names = []
-    pets_info_dict = {}
+    pets_list = []
     for i in range(len(actual_pets)):
-        pet_info = actual_pets[i].text[:-2].split(' ') # Если брать полную строку, то возникает ошибка кодировки
-        assert len(pet_info) == 3, "не у всех питомцев есть имя, возраст и порода"
+        pet_info = actual_pets[i].text[:-2].split('\n')
+        assert len(pet_info[0].split()) == 3, "не у всех питомцев есть имя, возраст и порода"  # задание 3
         pets_names.append(pet_info[0])
-        pets_info_dict[i + 1] = pet_info
+        pets_list.append(" ".join(pet_info))
+    # print(f'\n{pets_list=}')
 
-        # assert pet_info[0] != ''
-        # assert pet_info[1] != ''
-        # assert pet_info[2] != ''
-
-
-    duplicate_names = []
-    no_double_names_flag = True
-    for item in pets_names:
-        if item not in duplicate_names:
-            duplicate_names.append(item)
-        else:
-            no_double_names_flag = False
-    assert no_double_names_flag, "не у всех питомцев разные имена"
-
-
-    print(f'{pets_info_dict=}')
-    print(f'{pets_info_dict.values()=}')
-
-
+    pet_names = [elem.split()[0] for elem in pets_list]
+    assert len(pet_names) == len(set(pet_names)), 'не у всех питомцев разные имена.'  # задание 4
+    assert len(pets_list) == len(set(pets_list)), 'Есть повторяющиеся питомцы'  # задание 5
