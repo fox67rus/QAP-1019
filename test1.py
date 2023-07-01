988a57b77704d4a59257f54b094f6a67886e6cb4
Elena <fox67rus@mail.ru>
2023-07-01 13:59:21 +0300

    ожидания

--- test1.py
+++ test1.py
@@ -8,6 +8,8 @@ from selenium import webdriver
 from selenium.webdriver import Keys, ActionChains
 from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 @pytest.fixture(autouse=True)
@@ -24,15 +26,17 @@ def testing():
 
 
 def test_search_fruto_nyanya():
-    sleep(3)
+
     pytest.driver.implicitly_wait(10)
-    search = pytest.driver.find_element(By.ID, "searchInput")
+    # WebDriverWait(pytest.driver, 10).until(EC.presence_of_all_elements_located
+    sleep(3)
+    search = WebDriverWait(pytest.driver, 10).until(EC.visibility_of_element_located((By.ID, "searchInput")))
     query_text = 'фруктовое пюре в мягкой упаковке фрутоняня'
     search.send_keys(query_text)
-    sleep(1)
     search.send_keys(Keys.RETURN)
-    assert pytest.driver.find_element(By.TAG_NAME, "h1").text == f"По запросу «{query_text}» найдено"
     sleep(5)
+    assert pytest.driver.find_element(By.TAG_NAME, "h1").text == f"По запросу «{query_text}» найдено"
+
 
 
 
@@ -41,13 +45,14 @@ def test_search_fruto_nyanya():
     # ActionChains(pytest.driver) \
     #     .scroll_from_origin(scroll_origin, 0, 200) \
     #     .perform()
-    prices = pytest.driver.find_elements(By.CLASS_NAME, "price__lower-price")
-    print(len(prices))
-    price_list = []
-    for el in prices:
-        price_list.append(el.text)
 
-    print(price_list)
+    # prices = pytest.driver.find_elements(By.CLASS_NAME, "price__lower-price")
+    # print(len(prices))
+    # price_list = []
+    # for el in prices:
+    #     price_list.append(el.text)
+    #
+    # print(price_list)
 
 
 
